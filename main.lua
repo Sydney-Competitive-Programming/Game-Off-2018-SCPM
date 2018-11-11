02ae77803b8b84d199ed214583f67657f635286d
kmiller96 <kale.miller96@gmail.com>
2018-11-11 20:09:55 +1100

    Added some boilerplate code

---
+++ main.lua
@@ -0,0 +1,21 @@
+--
+-- Github Game Off
+---
+--- Created by the team at Sydney Competitive Programming Meetup.
+
+Object = require "src/classic"
+
+
+function love.load()
+  --|| The master loading function ||--
+end
+
+
+function love.update(dt)
+  --|| The master update function ||--
+end
+
+
+function love.draw()
+  --|| The master drawing function ||--
+end
