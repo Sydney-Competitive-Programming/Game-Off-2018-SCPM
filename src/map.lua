8096e15ea4f8316bb875259d4c7bc83b061578ee
kmiller96 <kale.miller96@gmail.com>
2018-11-16 22:27:02 +1100

    Refactored code

--- src/map.lua
+++ src/map.lua
@@ -6,14 +6,18 @@
 -- HELPER FUNCTIONS --
 ----------------------
 
-local function scalingFactor(tilemap, canvas_width, canvas_height)
+local function getScalingFactor(tilemap, canvas_width, canvas_height)
   --|| Computes the scaling factor for the drawing ||--
-  local width, height = TILESET[1]:getDimensions()
-  local total_draw_width = width*table.getn(tilemap[1])
-  local total_draw_height = height*table.getn(tilemap)
+  local tile_width, tile_height = TILESET[1]:getDimensions()
+  local ntiles_width = table.getn(tilemap[1])
+  local ntiles_height = table.getn(tilemap)
+
+  local total_draw_width = tile_width*ntiles_width
+  local total_draw_height = tile_height*ntiles_height
 
   local sx = canvas_width/total_draw_width
   local sy = canvas_height/total_draw_height
+
   return sx, sy
 end
 
@@ -30,6 +34,7 @@ TILESET = {
 -- ARENAS DECLARATION --
 ------------------------
 
+-- Example Arena 1 -- Used for testing --
 EXAMPLE_ARENA_1 = {
   {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,},
   {2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,},
@@ -53,31 +58,26 @@ function Map:new(tilemap, canvas_width, canvas_height)
   --|| Initalises the game map ||--
   self.tilemap = tilemap
   self.tile_width, self.tile_height = TILESET[1]:getDimensions()
-  self.sx, self.sy = scalingFactor(tilemap, canvas_width, canvas_height)
+  self.sx, self.sy = getScalingFactor(tilemap, canvas_width, canvas_height)
 end
 
 
 function Map:update(dt)
   --|| Updates the game map ||--
-  -- Nothing is called here as the map remains static --
+  -- Nothing is called here as the map currently remains static --
 end
 
 
 function Map:draw()
   --|| Draws the game map ||--
-
-  -- Figure out the scaling factor for the map so it fits the canvas.
-
-
-  -- Finally draw the canvas.
   for y, y_val in ipairs(self.tilemap) do
     for x, x_val in ipairs(y_val) do
-      local tile = TILESET[x_val]
       love.graphics.draw(
-        tile,
+        TILESET[x_val],  -- Draw the tile as per tilemap key
         (x-1)*self.tile_width*self.sx, -- Xpos
         (y-1)*self.tile_height*self.sy,  -- Ypos
-        0, self.sx, self.sy
+        0,  -- Rotation
+        self.sx, self.sy  -- Scaling
       )
     end
   end
