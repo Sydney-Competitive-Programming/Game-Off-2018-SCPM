7867d49bb4cafa36627d9769f12dac85d26db1ff
ademi <ademi.muhammad@gmail.com>
2018-11-19 21:55:39 +1100

    1) Instead of one enemy, the game class owns a list of enemies that start 	at the top of the game in random horizontal locations, and travel 	downward
    2) when an enemy corsses the canvas borders the game context class destroys it

--- src/game_context.lua
+++ src/game_context.lua
@@ -14,13 +14,23 @@ function Game_Context:new()
     self.Game_field = Canvas()
 
     
-    -- initiate player and an enemy
+    -- initiate player and 
     require "src/entity"
     require "src/example_player"
     self.player = self.create_player(self,self.Game_field.width/2,self.Game_field.height,10,500);
     
+    -- initiate enemies
     require "src/example_enemy"
-    self.enemy = self.create_enemy(self,self.Game_field.width/2,0,70)
+    math.randomseed(os.time())
+    self.enemy_number =4
+    self.enemies={}
+
+    for e =1,self.enemy_number do
+        -- randomly choose a position for the enemy witihin 10%-90% of the width of the canvas
+        local x_pos = math.random(self.Game_field.width*0.1,self.Game_field.width*0.9)
+        self.enemies[e]= self.create_enemy(self,x_pos,0,70)
+    end
+    
 
 end
 
@@ -28,7 +38,10 @@ function Game_Context:update(frame_rate , key_counter)
 
     self.Game_field:update()
     self.player:update(frame_rate,key_counter*frame_rate)
-    self.enemy:update(frame_rate)
+    
+    for i,e in ipairs(self.enemies)do
+        e:update(frame_rate)
+    end
 
     -- Ensure the player stays within the game field boundary
     if self.player.x < 0 then 
@@ -45,13 +58,28 @@ function Game_Context:update(frame_rate , key_counter)
         self.player.y = self.Game_field.height-self.player.h 
     end
 
+    -- if an enemy gets out of the canvas box kill it
+    for i,e in ipairs(self.enemies) do
+        if e.x < 0 
+            or (e.x+e.w) > self.Game_field.width
+            or e.y < 0
+            or (e.y+e.h) > self.Game_field.height
+            then
+                table.remove(self.enemies,i)
+                --print(#self.enemies)
+        end
+
+    end
 end
+
 
 function Game_Context:draw()
 
     self.Game_field:draw()
     self.player:draw()
-    self.enemy:draw()
+    for i,e in ipairs(self.enemies)do
+        e:draw(frame_rate)
+    end
 end
 
 
@@ -61,9 +89,9 @@ function Game_Context:create_player(init_x,init_y,init_speed,acceleration)
     ,50,50,init_speed,acceleration,500)
     return player
 end
-function Game_Context:create_enemy(init_x,init_y,speed)
+function Game_Context:create_enemy(pos_x,pos_y,speed)
     enemy = Enemy(
-        init_x,init_y
+        pos_x,pos_y
         ,50,50,speed)
     return enemy
 end
